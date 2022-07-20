5d2ec6a8e75ba150b0fd578e47b4b55f07172e54
Avenger <smd984570656@126.com>
2022-07-20 15:52:32 +0800

    Update index.tsx

--- html/src/index.tsx
+++ html/src/index.tsx
@@ -2,5 +2,5 @@ import 'whatwg-fetch';
 import { h, render } from 'preact';
 import { App } from './components/app';
 import './style/index.scss';
-
+console.log("111")
 render(<App />, document.body);
